9712a3485bb095fed568692dcbbcafbb50148a07
Sekhar <sirivuri@adobe.com>
2023-08-23 11:19:35 -0600

    Adding response parsing and application json request header

--- libs/blocks/firefly/firefly-api-conversations.js
+++ libs/blocks/firefly/firefly-api-conversations.js
@@ -1,15 +1,9 @@
+const regex = /^\d+\./gm;
 const conversationOptions = {
   method: 'POST',
   headers: {},
   body: '{\n    "messages": [{\n        "role": "system",\n        "content": "You are an AI assistant that helps people find information."\n    }],\n    "llm_metadata": {\n        "model_name": "gpt-35-turbo",\n        "llm_type": "azure_chat_openai",\n        "temperature": 0.7,\n        "max_tokens": 800,\n        "top_p": 0.95,\n        "frequency_penalty": 0,\n        "presence_penalty": 0\n    },\n    "number_of_past_messages_to_include": 5,\n    "data_retention_days": 7\n}',
 };
-
-// const headersFunction = async ({ accessToken, x-api-key, ims-org-id }) => {
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//     },
-//   };
 
 const conversationsApi = async () =>
   // eslint-disable-next-line no-return-await, implicit-arrow-linebreak
@@ -18,28 +12,29 @@ const conversationsApi = async () =>
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
 
-function queryConversationsOptions(article) {
+function queryConversationsOptions(article, qty) {
   const options = {
     method: 'POST',
     headers: {},
-    body: `{"conversation_identifier": 825926,"messages":[{"role": "user","content": "${article}"}]}`,
+    body: `{"conversation_identifier": 825926,"messages":[{"role": "user","content": "List ${qty} keywords from ${article}"}]}`,
   };
   return options;
 }
 
-export async function queryConversations(article) {
+async function queryConversations(article, qty) {
   // eslint-disable-next-line no-return-await, implicit-arrow-linebreak
-  await fetch('https://firefall-stage.adobe.io/v1/chat/completions', queryConversationsOptions(article))
+  return await fetch('https://firefall-stage.adobe.io/v1/chat/completions', queryConversationsOptions(article, qty))
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((data) => data?.generations[0]?.[0]?.message?.content.split('\n').map((eachKeyword) => eachKeyword.replace(regex, '')))
     .catch((err) => console.error(err));
 }
 
 async function init() {
   try {
     console.log('test!!!');
-    // conversationsApi(); //testing with just query APIs
-    queryConversations("List Keywords from Take the hassle out of collecting customer data with web forms they can fill out fast. Quickly edit PDF agreements and proposals and send them for e-signature to close the deal. With Acrobat, it's a walk in the park.");
+    //conversationsApi(); //testing with just query APIs
+    const keywords = await queryConversations('Enterprises are adopting Express to enhance the productivity of creative teams and empower marketing organizations to quickly and easily create on-brand content that stands out', 3);
+    console.log(keywords);
   } catch {
     // leave it blank for now
   }
